51acd440bc1160458863317e31f0f4d4f8659e80
XDR <xxdr@duck.com>
2024-09-21 12:17:37 +0000

    chore: update/fix install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ home_dir="$HOME"
 
 echo "WARNING: Please ensure you have installed the necessary dependencies before running this script."
 echo "Required dependencies: curl, fish, sway, waybar, tofi, kitty, cliphist, swaybg, swayidle, xdg-user-dirs, swaylock, grim, wl-clipboard, slurp, mako, wtype, xdg-utils, autotiling-rs, pavucontrol, xdg-desktop-portal-wlr, polkit-gnome"
+echo "Note: to control audio via keyboard or waybar, you need pipewire and wireplumber"
 echo "Do you want to continue? (y/n)"
 read -r response
 
@@ -12,12 +13,10 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     cp -r "$current_dir"/. "$home_dir/"
 
-    rm -f "$home_dir/$(basename "$0")" "$home_dir/.gitignore" "$home_dir/.README.md"
+    rm -rf "$home_dir/$(basename "$0")" "$home_dir/.gitignore" "$home_dir/.README.md" "$home_dir/.git"
     echo "Dotfiles copied successfully."
 
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-
-    fish -c "omf install boxfish"
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish -c "omf install boxfish"
     echo "Oh My Fish and the 'boxfish' theme have been installed."
 
     if ! grep -q "$(which fish)" /etc/shells; then
@@ -25,9 +24,8 @@ then
     fi
     chsh -s "$(which fish)"
     echo "Fish is now your default shell."
+    echo "Thanks for using my dots - XDR"
     exit
 else
     echo "Operation cancelled."
 fi
-
-# This script hasn't been tested :skull:
